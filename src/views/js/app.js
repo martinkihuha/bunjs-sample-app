f846184213e4b66747526b2c18cc4dd66fbbd8c4
Martin Kihuha <mkihuha@gmail.com>
2026-08-04 08:09:44 +0300

    feat: enable htmx sidebar navigation and bundle global view transitions

--- src/views/js/app.js
+++ src/views/js/app.js
@@ -1,4 +1,7 @@
-import 'htmx.org';
+import htmx from 'htmx.org';
 import "basecoat-css/all";
 import 'iconify-icon';
 import './charts';
+
+window.htmx = htmx;
+htmx.config.globalViewTransitions = true;
